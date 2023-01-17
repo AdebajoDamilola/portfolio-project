dbd30ec067a33c85b7ac7888f683a59a7891cbc1
AdebajoDamilola <adebajoluwadamilola@gmail.com>
2023-01-17 01:51:50 +0100

    new commit

---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import "./FooterStyle.css";
+
+import React from "react";
+import { FaFacebook, FaHome, FaMailBulk, FaPhone, FaTwitter,FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+            <div>
+              <p>Lagos.</p>
+              <p>Nigeria</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              +234-000-000-0000
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              email@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+          <h4>About</h4>
+          <p>I am a frontEnd Developer. I love implementing beautiful websites using HTML, CSS, JAVSCRIPT, REACT.</p>
+          <div className="social">
+            <FaFacebook
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaTwitter
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#fff", marginRight: "1rem" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,21 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Random Robots app",
+    text: "This web app shows random robot pictures, names and email. The name of robot can be searched using the search input box",
+    view: "/",
+  },
+  {
+    imgsrc: pro2,
+    title: "Review Application",
+    text: "Review Application is used to give rating and reviews as well as read other people's rating and review about a product or company. You can also see the total reviews and average rating ",
+    view: "/",
+  },
+];
+
+export default ProjectCardData;
